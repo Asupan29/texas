2178c895d6bb111a917537cf0f1026c1a87cb22a
Asupan29 <138056817+Asupan29@users.noreply.github.com>
2023-12-20 13:32:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6796047745:AAHkBzofOnbb_yrvSGGlLDHgHawHrJUzGd8")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6323015158:AAHfjqb_bxlU_jnlwFKhchFnykm49sMjlcE")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "26543176"))
@@ -36,8 +36,8 @@ PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "-1001922217206"))
-FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "-1001360715875"))
-FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "0"))
+FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "-1002111889282"))
+FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "-1001976777981"))
 FORCE_SUB_4 = int(os.environ.get("FORCE_SUB_4", "0"))
 FORCE_SUB_5 = int(os.environ.get("FORCE_SUB_5", "0"))
 FORCE_SUB_6 = int(os.environ.get("FORCE_SUB_6", "0"))
